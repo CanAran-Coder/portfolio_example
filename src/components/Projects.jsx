11e4555e7f1c84db2e478b4f0a350c8fd5d608bc
Can Aran <Can>
2025-11-08 11:50:56 +0300

    fix:Solve Alt Image Problem For Production

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ function Projects() {
                     <div className="Project">
                         <h3 className="portfolioHeader">Can's Coffe Shop</h3>
                         <p className="portfolioText">In this project, I've learnt how backend and frontend communicate each other.I used Html, Css, Bootstrap, Javascript,MySQL, React, Node js etc.You can sign up and login, buy products with payment , get contact with live support. </p>
-                        <button className="githubButton">See In Github <img className="githubIcon" src="/images/github.png"></img> </button>
+                        <button className="githubButton">See In Github <img alt="githubImage" className="githubIcon" src="/images/github.png"></img> </button>
                     </div>
                 </div>
             </div>
